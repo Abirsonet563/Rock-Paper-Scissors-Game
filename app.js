5af7e07c41801980c1c7115b1aaff771463c0283
Abir Hasan <abir.hasan@pickles.com.au>
2024-10-11 19:25:42 +0100

    Implement Compare Function by Use If Else Condition

--- app.js
+++ app.js
@@ -23,13 +23,57 @@ const game = () =>{
 
         options.forEach(option => {
          option.addEventListener("click", function(){
-            // console.log(this);
+            // Computer Choice
             const computerNumber = Math.floor(Math.random() * 3);
             const computerChoice = computerOption[computerNumber];
-            console.log(computerChoice);
+            // Here is Where We Call Compare Hands
+
+
+            //Update Images
+            playerHand.src = `./assets/${this.textContent}.png`;
+            computerHand.src = `./assets/${computerChoice}.png`;
          });
         });
 
+        const compareHand = (playerChoice, computerChoice) => {
+          //Update Text
+          const winner = document.querySelector('.winner');
+          //Cheaking for a Tie
+          if(playerChoice === computerChoice){
+            winner.textContent = 'It is a tie';
+            return;
+          } 
+          //Check For Rock
+          if(playerChoice === 'rock'){
+            if(computerChoice === 'scissors'){
+                winner.textContent = 'Players Wins'
+                return;
+            }else{
+                winner.textContent = 'Computer Wins'
+                return;
+            }
+          }
+                    //Check For Paper
+                    if(playerChoice === 'paper'){
+                        if(computerChoice === 'scissors'){
+                            winner.textContent = 'Computer Wins'
+                            return;
+                        }else{
+                            winner.textContent = 'Players Wins'
+                            return;
+                        }
+                      }
+                  //Check For Scissors
+                  if(playerChoice === 'scissors'){
+                    if(computerChoice === 'rock'){
+                        winner.textContent = 'Computer Wins'
+                        return;
+                    }else{
+                        winner.textContent = 'Players Wins'
+                        return;
+                    }
+                  }
+        }
     }
     //Is Call the Inner Function
     startGame();
