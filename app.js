5a5960e443ae8704ce1d9f080b3ac70d75aeeaaf
Abir Hasan <abir.hasan@pickles.com.au>
2024-10-11 23:30:20 +0100

    Add Space to Code and Add setTimeout Function

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const game = () =>{
+const game = () => {
     let pScore = 0;
     let cScore = 0;
 
+    //Start the Game
     const startGame = () =>{
         const playBtn = document.querySelector('.intro button');
         const introScreen = document.querySelector('.intro');
@@ -19,36 +20,41 @@ const game = () =>{
         const computerHand = document.querySelector('.computer-hand');
         const hands = document.querySelectorAll('.hands img');
 
-        hands.forEach(hand =>{
-            hand.addEventListener('animationend', function(){
+        hands.forEach(hand => {
+            hand.addEventListener('animationend', function() {
                 this.style.animation = '';
             });
-        })
+        });
         //Computer Option
         const computerOption = ["rock", "paper", "scissors"];
 
         options.forEach(option => {
-         option.addEventListener("click", function(){
+         option.addEventListener("click", function() {
             // Computer Choice
             const computerNumber = Math.floor(Math.random() * 3);
             const computerChoice = computerOption[computerNumber];
+
+            setTimeout(()=>{
             // Here is Where We Call Compare Hands
             compareHands(this.textContent, computerChoice);
             //Update Images
             playerHand.src = `./assets/${this.textContent}.png`;
             computerHand.src = `./assets/${computerChoice}.png`;
+            }, 2000);
             //Add Animation
             playerHand.style.animation = "shakePlayer 2s ease";
             computerHand.style.animation = "shakeComputer 2s ease";
+        
          });
         });
-       
+    };
         const updateScore = () => {
             const playerScore = document.querySelector('.player-score p');
             const computerScore = document.querySelector('computer-score p');
             playerScore.textContent = pScore;
             computerScore.textContent = cScore;
-        }
+        };
+
         const compareHands = (playerChoice, computerChoice) => {
           //Update Text
           const winner = document.querySelector(".winner");
@@ -99,8 +105,8 @@ const game = () =>{
                         return;
                     }
                   }
-        }
-    }
+        };
+    
     //Is Call the Inner Function
     startGame();
     playMatch();
