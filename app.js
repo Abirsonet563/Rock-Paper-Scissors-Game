0806cb3cd7c2ebe816117ea1c8f5b48d55aeb7dd
Abir Hasan <abir.hasan@pickles.com.au>
2024-10-11 10:48:34 +0100

    Implement Computer Choice Function by Add Random Number

--- app.js
+++ app.js
@@ -10,11 +10,30 @@ const game = () =>{
         playBtn.addEventListener('click',() =>{
            introScreen.classList.add('fadeOut');
            match.classList.add('fadeIn');
-        })
+        });
+    };
+   //play Match
+    const playMatch = () => {
+        const options = document.querySelectorAll('.options button');
+        const playerHand = document.querySelector('.player-hand')
+        const computerHand = document.querySelector('.computer-hand');
+
+        //Computer Option
+        const computerOption = ["rock", "paper", "scissors"];
+
+        options.forEach(option => {
+         option.addEventListener("click", function(){
+            // console.log(this);
+            const computerNumber = Math.floor(Math.random() * 3);
+            const computerChoice = computerOption[computerNumber];
+            console.log(computerChoice);
+         });
+        });
+
     }
     //Is Call the Inner Function
     startGame();
-    updateScore(); 
+    playMatch();
 }
 
 //Start the Game Function
